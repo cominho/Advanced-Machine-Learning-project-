92cbbf298f993fc348b1b988662ad9e8ecee8966
leorene97490 <leo@dfi-labs.com>
2025-01-14 12:40:09 +0100

    [ADD] rolling universe

--- adv_machine/config.py
+++ adv_machine/config.py
@@ -5,3 +5,5 @@ import adv_machine.utils as ut
 df = pd.read_csv(f'available_stock.csv',delimiter=';')
 df.index = df['Ticker']
 ticker_to_screen = df.to_dict(orient='index')
+
+available_stocks = list(ticker_to_screen.keys())
